886267e69e21f08b41958d3ccb1946a954f39543
Tle <title1337@gmail.com>
2026-03-22 11:04:18 +0700

    Feat : EX1

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,7 @@
 // Exercise #1: Decrease the Student Scores
 let scores = [89, 70, 60, 50, 100, 71];
-
+for (let i = 0; i < scores.length; i++) {
+    scores[i] = scores[i] - 10;
+}
+console.log(scores);
 // Start coding here
