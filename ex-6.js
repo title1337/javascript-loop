2eb0fd1afe2954e617ab467b218f1b335e681831
Tle <title1337@gmail.com>
2026-03-22 11:30:39 +0700

    Feat : EX6

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,7 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+for (let i =0; i < companyName.length; i++) {
+    reversedCompanyName = companyName[i] + reversedCompanyName;
+}
 console.log(reversedCompanyName);
