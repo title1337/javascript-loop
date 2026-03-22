a9ffc2582cc2d3b131d0fd0099deb1fd31b20499
Tle <title1337@gmail.com>
2026-03-22 11:11:56 +0700

    Feat : EX3

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,10 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
-let minScore;
-// Start coding here
-
+let minScore = scores[0];
+for (let i = 1; i < scores.length; i++) {
+    if (scores[i] < minScore) {
+        minScore = scores[i];
+    }   
+}
 console.log(minScore);
+
